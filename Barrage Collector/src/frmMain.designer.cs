f17ecde73dec60d1d6304d1fd6790034e5fb0d51
JackGeminiKU <JackGeminiKu@gmail.com>
2018-06-12 21:57:19 +0800

    重构

--- Barrage Collector/src/frmMain.designer.cs
+++ Barrage Collector/src/frmMain.designer.cs
@@ -37,6 +37,7 @@
             // barragePanel
             // 
             this.barragePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.barragePanel.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.barragePanel.Location = new System.Drawing.Point(0, 0);
             this.barragePanel.Name = "barragePanel";
             this.barragePanel.Size = new System.Drawing.Size(756, 462);
